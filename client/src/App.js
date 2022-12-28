2fbd5ea94bd420503805d2c9c20f0ae921aca619
Rutherson <rutherson20@gmail.com>
2022-12-28 14:02:12 +0800

    setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Cart from './pages/cart';
+import Checkout from './pages/checkout';
+import NotFound from './pages/error/NotFound';
+import Home from './pages/home';
+import Inventory from './pages/inventory';
+import Transactions from './pages/transactions';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from './layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<Router>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route
+							index
+							element={<Home/>}
+						/>
+						<Route
+							path="cart"
+							exact
+							element={<Cart />}
+						/>
+						<Route
+							path="checkout"
+							exact
+							element={<Checkout />}
+						/>
+            <Route
+							path="inventory"
+							exact
+							element={<Inventory />}
+						/>
+            <Route
+							path="transactions"
+							exact
+							element={<Transactions />}
+						/>
+						<Route
+							path="*"
+							exact
+							element={<NotFound />}
+						/>
+					</Route>
+				</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/pages/inventory/index.js
@@ -0,0 +1,9 @@
+function Inventory() {
+  return (
+    <div>
+        Inventory
+    </div>
+  );
+}
+
+export default Inventory;
