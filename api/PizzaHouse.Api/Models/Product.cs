627d19afdd0097f8ead7413075c339e2dfde15ca
rutherson <rutherson20@gmail.com>
2023-01-02 23:23:06 +0800

    models

---
+++ api/PizzaHouse.Api/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace PizzaHouse.Api.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public int InStock { get; set; }
+    public decimal Price { get; set; }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public decimal TotalCost { get; set; }
+    public int ProductCount { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+}
+
+public class OrderDetails //Cart
+{
+    public int Id { get; set; }
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
